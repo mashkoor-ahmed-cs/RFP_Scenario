2c826f8b58ca98d08840a3f79544595579de87e8
Mashkoor Ahmed <mashkoor_ahmed@hotmail.co.uk>
2017-12-11 23:23:56 +0000

    penultimate commit for shape drawer class

--- src/com/company/ShapeDrawer.java
+++ src/com/company/ShapeDrawer.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.geom.Path2D;
 import java.util.ArrayList;
 
-public class ShapeDrawer {
+public class ShapeDrawer extends JPanel{
     private Path2D path = new Path2D.Double();
     private ArrayList<Coordinate> points;
 
@@ -12,11 +14,23 @@ public class ShapeDrawer {
     }
 
     public void drawShape(){
+
         path.moveTo(points.get(0).getX(), points.get(0).getY());
         for (int i = 1; i < points.size(); i++){
             path.lineTo(points.get(i).getX(), points.get(i).getY());
         }
         path.closePath();
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // come back to this 
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.draw(path);
+        g2d.dispose();
+
     }
 
 }
