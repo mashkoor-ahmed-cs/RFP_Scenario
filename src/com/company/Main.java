b5929cfa5675e54a383ee230ca61834632c594d6
Mashkoor Ahmed <mashkoor_ahmed@hotmail.co.uk>
2017-12-11 12:57:04 +0000

    Extracted ReadTextFile into its own class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,31 +6,12 @@ import java.util.ArrayList;
 
 public class Main {
 
-    private ArrayList<String> questions = new ArrayList<String>();
-    public String problemFileName = "problems.rfp";
-
-    private void readFile(){
-        BufferedReader reader = new BufferedReader(new FileReader(problemFileName));
-        String line;
-        try{
-            while ((line = reader.readLine()) != null)
-            {
-                questions.add(line);
-            }
-            reader.close();
-        }
-        catch (Exception e){
-            System.err.format("Exception occurred trying to read '%s'.", problemFileName);
-            e.printStackTrace();
-        }
-
-    }
-
     public static void main(String[] args) {
-	// write your code here
         System.out.println("There are 30 problems to solve");
-        readFile();
+        String problemFileName = "problems.rfp";
 
+        ReadTextFile readTextFile = new ReadTextFile(problemFileName);
+        ArrayList<String> questions = readTextFile.getQuestions();
 
     }
 }
---
+++ src/com/company/ReadTextFile.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class ReadTextFile {
+
+    private ArrayList<String> questions = new ArrayList<String>();
+
+    public ReadTextFile(String filename){
+        /* Sets up a new file reader
+        goes through each line of text file and then adds that to our list of 30 questions */
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        try{
+            while ((line = reader.readLine()) != null)
+            {
+                questions.add(line);
+            }
+            reader.close();
+        }
+        catch (Exception e){
+            System.err.format("Exception occurred trying to read '%s'.", filename);
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getQuestions() {
+        return questions;
+    }
+
+}
