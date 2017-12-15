fbc08bbf8959cee2b68029a05c82b1dfdf3a5b4d
Mashkoor Ahmed <mashkoor_ahmed@hotmail.co.uk>
2017-12-15 12:16:49 +0000

    added scrollbar implementation to main repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -41,12 +41,23 @@ public class Main {
         writer.closeFile();
 
         JFrame frame = new JFrame("Visualiser");
+        frame.setLayout(new BorderLayout());
+        ShapeDrawer drawProblem = new ShapeDrawer(problems.get(problemToShow - 1));
+        JScrollPane scrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        int ScrollUnit = 50;
+        int XDimension = 4000; // needs to be variable to room + shapes - or just really big
+        int YDimension = 4000; // ideally just needs to be height of room + a bit
 
+        scrollPane.getVerticalScrollBar().setUnitIncrement(ScrollUnit);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(ScrollUnit);
+        drawProblem.setPreferredSize(new Dimension(XDimension,YDimension));
+        scrollPane.add(drawProblem);
+        scrollPane.setViewportView(drawProblem);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new ShapeDrawer(problems.get(problemToShow - 1)));
         frame.pack();
         frame.setSize(new Dimension(1200, 600));
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
         frame.setVisible(true);
     }
 }
